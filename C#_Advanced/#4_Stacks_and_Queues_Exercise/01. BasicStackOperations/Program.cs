bae73975bf64a4297d28953512fd4acaf0db70a5
unknown <blgv@abv.bg>
2021-08-23 07:55:05 +0300

    Added some Exercises - Stack and Queues

---
+++ C#_Advanced/#4_Stacks_and_Queues_Exercise/01. BasicStackOperations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int pushElements = arr[0];
+            int popElements = arr[1];
+            int element = arr[2];
+
+            Stack<int> numbers = new Stack<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse));
+
+            for (int i = 0; i < popElements; i++)
+            {
+                numbers.Pop();
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine(numbers.Count);
+                return;
+            }
+            else if (numbers.Contains(element))
+            {
+                Console.WriteLine("true");
+                return;
+            }
+
+            Console.WriteLine(numbers.Min());
+        }
+    }
+}
---
+++ C#_Advanced/#4_Stacks_and_Queues_Exercise/02. BasicQueueOperations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int pushElements = arr[0];
+            int popElements = arr[1];
+            int element = arr[2];
+
+            Queue<int> numbers = new Queue<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse));
+
+            for (int i = 0; i < popElements; i++)
+            {
+                numbers.Dequeue();
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine(numbers.Count);
+                return;
+            }
+            else if (numbers.Contains(element))
+            {
+                Console.WriteLine("true");
+                return;
+            }
+
+            Console.WriteLine(numbers.Min());
+        }
+    }
+}
