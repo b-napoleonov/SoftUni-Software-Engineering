b7fc93d9973238c3dbedf55726c1664a60ea7211
unknown <blgv@abv.bg>
2021-08-23 07:47:47 +0300

    Added C# Advanced Folder and Stack @ Queues Lab

---
+++ C#_Advanced/#3_Stacks_and_Queues_Lab/7. HotPotato/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> children = new Queue<string>(Console.ReadLine().Split());
+
+            int n = int.Parse(Console.ReadLine());
+
+            while (children.Count > 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    children.Enqueue(children.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {children.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {children.Dequeue()}");
+        }
+    }
+}
---
+++ C#_Advanced/#3_Stacks_and_Queues_Lab/8. TrafficJam/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> cars = new Queue<string>();
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                if (input != "green")
+                {
+                    cars.Enqueue(input);
+                }
+                else
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        string car = string.Empty;
+
+                        if (cars.TryDequeue(out car))
+                        {
+                            Console.WriteLine($"{car} passed!");
+                            counter++;
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
