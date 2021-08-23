162c01bb06ed9b9ae133e4e31235afc958f88d7e
unknown <blgv@abv.bg>
2021-08-23 07:53:28 +0300

    Added Stacks and Queues Lab

---
+++ C#_Advanced/#3_Stack_and_Queues_Lab/5. PrintEvenNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> que = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            List<int> numbers = new List<int>(que.Count);
+
+            foreach (var item in que)
+            {
+                if (item % 2 == 0)
+                {
+                    numbers.Add(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
