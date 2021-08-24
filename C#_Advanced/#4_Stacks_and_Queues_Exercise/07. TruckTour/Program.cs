61a86ed7951bc59620d63f6aa13f9cba96405dfa
unknown <blgv@abv.bg>
2021-08-24 17:05:30 +0300

    Updated Exercise 7 of Stacks and Queues

--- C#_Advanced/#4_Stacks_and_Queues_Exercise/07. TruckTour/Program.cs
+++ C#_Advanced/#4_Stacks_and_Queues_Exercise/07. TruckTour/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace _07._TruckTour
 {
@@ -17,6 +16,48 @@ namespace _07._TruckTour
             {
                 circle.Enqueue(Console.ReadLine());
             }
+
+            int index = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                long petrol = 0;
+                bool isEnough = true;
+
+                for (int j = 0; j < n; j++)
+                {
+                    string currentPump = circle.Dequeue();
+                    long[] pumpValue = currentPump
+                        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                        .Select(long.Parse)
+                        .ToArray();
+
+                    long currentPetrol = pumpValue[0];
+                    long distance = pumpValue[1];
+                    petrol += currentPetrol;
+
+                    if (petrol >= distance)
+                    {
+                        petrol -= distance;
+                    }
+                    else
+                    {
+                        isEnough = false;
+                    }
+
+                    circle.Enqueue(currentPump);
+                }
+
+                if (isEnough)
+                {
+                    index = i;
+                    break;
+                }
+
+                circle.Enqueue(circle.Dequeue());
+            }
+
+            Console.WriteLine(index);
         }
     }
 }
