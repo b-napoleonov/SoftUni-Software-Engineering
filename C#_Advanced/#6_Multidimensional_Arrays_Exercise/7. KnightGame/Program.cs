b751ed180be82f90269106d6751d0b1f6164f577
unknown <blgv@abv.bg>
2021-08-26 11:59:51 +0300

    Updated Exercise 7 - Knight Game

--- C#_Advanced/#6_Multidimensional_Arrays_Exercise/7. KnightGame/Program.cs
+++ C#_Advanced/#6_Multidimensional_Arrays_Exercise/7. KnightGame/Program.cs
@@ -6,8 +6,54 @@ namespace _7._KnightGame
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            int dimensions = int.Parse(Console.ReadLine());
 
+            char[,] matrix = MatrixReader(dimensions);
+
+            int knightsInDanger = 0;
+            int maxInDanger = 0;
+            int knightRow = 0;
+            int knightCol = 0;
+            int removeCount = 0;
+
+            while (true)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        if (matrix[row, col] == 'K')
+                        {
+                            knightsInDanger = InDangerChecker(dimensions, matrix, knightsInDanger, row, col);
+                        }
+
+                        if (knightsInDanger > maxInDanger)
+                        {
+                            maxInDanger = knightsInDanger;
+                            knightRow = row;
+                            knightCol = col;
+                        }
+
+                        knightsInDanger = 0;
+                    }
+                }
+
+                if (maxInDanger > 0)
+                {
+                    matrix[knightRow, knightCol] = '0';
+                    removeCount++;
+                    maxInDanger = 0;
+                }
+                else
+                {
+                    Console.WriteLine(removeCount);
+                    return;
+                }
+            }
+        }
+
+        private static char[,] MatrixReader(int n)
+        {
             char[,] matrix = new char[n, n];
 
             for (int i = 0; i < matrix.GetLength(0); i++)
@@ -20,102 +66,45 @@ namespace _7._KnightGame
                 }
             }
 
-            int knightsInDanger = 0;
-            int maxInDanger = int.MinValue;
-            int row = 0;
-            int col = 0;
-            int removeCount = 0;
+            return matrix;
+        }
 
-            while (true)
+        private static int InDangerChecker(int n, char[,] matrix, int knightsInDanger, int row, int col)
+        {
+            if (row + 1 < n && col + 2 < n && matrix[row + 1, col + 2] == 'K')
             {
-
-                for (int i = 0; i < matrix.GetLength(0); i++)
-                {
-                    for (int j = 0; j < matrix.GetLength(1); j++)
-                    {
-                        if (matrix[i, j] == 'K')
-                        {
-                            if (i + 1 < n && j + 2 < n)
-                            {
-                                if (matrix[i + 1, j + 2] == 'K')
-                                {
-                                    knightsInDanger++;
-                                }
-                            }
-                            if (i + 2 < n && j + 1 < n)
-                            {
-                                if (matrix[i + 2, j + 1] == 'K')
-                                {
-                                    knightsInDanger++;
-                                }
-                            }
-                            if (i + 1 < n && j - 2 >= 0)
-                            {
-                                if (matrix[i + 1, j - 2] == 'K')
-                                {
-                                    knightsInDanger++;
-                                }
-                            }
-                            if (i + 2 < n && j - 1 >= 0)
-                            {
-                                if (matrix[i + 2, j - 1] == 'K')
-                                {
-                                    knightsInDanger++;
-                                }
-                            }
-                            if (i - 1 >= 0 && j + 2 < n)
-                            {
-                                if (matrix[i - 1, j + 2] == 'K')
-                                {
-                                    knightsInDanger++;
-                                }
-                            }
-                            if (i - 2 >= 0 && j + 1 < n)
-                            {
-                                if (matrix[i - 2, j + 1] == 'K')
-                                {
-                                    knightsInDanger++;
-                                }
-                            }
-                            if (i - 1 >= 0 && j - 2 >= 0)
-                            {
-                                if (matrix[i - 1, j - 2] == 'K')
-                                {
-                                    knightsInDanger++;
-                                }
-                            }
-                            if (i - 2 >= 0 && j - 1 >= 0)
-                            {
-                                if (matrix[i - 2, j - 1] == 'K')
-                                {
-                                    knightsInDanger++;
-                                }
-                            }
-                        }
-
-                        if (knightsInDanger > maxInDanger)
-                        {
-                            maxInDanger = knightsInDanger;
-                            row = i;
-                            col = j;
-                        }
-
-                        knightsInDanger = 0;
-                    }
-                }
-
-                if (maxInDanger != 0)
-                {
-                    matrix[row, col] = '0';
-                    removeCount++;
-                    maxInDanger = 0;
-                }
-                else
-                {
-                    Console.WriteLine(removeCount);
-                    return;
-                }
+                knightsInDanger++;
             }
+            if (row + 2 < n && col + 1 < n && matrix[row + 2, col + 1] == 'K')
+            {
+                knightsInDanger++;
+            }
+            if (row + 1 < n && col - 2 >= 0 && matrix[row + 1, col - 2] == 'K')
+            {
+                knightsInDanger++;
+            }
+            if (row + 2 < n && col - 1 >= 0 && matrix[row + 2, col - 1] == 'K')
+            {
+                knightsInDanger++;
+            }
+            if (row - 1 >= 0 && col + 2 < n && matrix[row - 1, col + 2] == 'K')
+            {
+                knightsInDanger++;
+            }
+            if (row - 2 >= 0 && col + 1 < n && matrix[row - 2, col + 1] == 'K')
+            {
+                knightsInDanger++;
+            }
+            if (row - 1 >= 0 && col - 2 >= 0 && matrix[row - 1, col - 2] == 'K')
+            {
+                knightsInDanger++;
+            }
+            if (row - 2 >= 0 && col - 1 >= 0 && matrix[row - 2, col - 1] == 'K')
+            {
+                knightsInDanger++;
+            }
+
+            return knightsInDanger;
         }
     }
 }
