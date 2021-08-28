11a5bb60a99068168d22a674c3bf3fabb8472c53
unknown <blgv@abv.bg>
2021-08-28 14:06:24 +0300

    Added rest of Sets and Dictionaries-Lab

---
+++ C#_Advanced/#7_Sets_and_Dictionaries_Advanced_Lab/06. ParkingLot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parkingLot = new HashSet<string>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] tokens = input
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0];
+                string carNumber = tokens[1];
+
+                switch (command)
+                {
+                    case "IN":
+                        parkingLot.Add(carNumber);
+                        break;
+
+                    case "OUT":
+                        parkingLot.Remove(carNumber);
+                        break;
+                }
+            }
+
+            if (parkingLot.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+
+            foreach (var car in parkingLot)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
---
+++ C#_Advanced/#7_Sets_and_Dictionaries_Advanced_Lab/07. SoftUniParty/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> VIP = new HashSet<string>();
+            HashSet<string> regular = new HashSet<string>();
+            bool isParty = false;
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (input == "PARTY")
+                {
+                    isParty = true;
+                    continue;
+                }
+                if (isParty == false)
+                {
+                    if (VIPChecker(input))
+                    {
+                        VIP.Add(input);
+                    }
+                    else
+                    {
+                        regular.Add(input);
+                    }
+                }
+                else
+                {
+                    if (VIPChecker(input))
+                    {
+                        VIP.Remove(input);
+                    }
+                    else
+                    {
+                        regular.Remove(input);
+                    }
+                }
+            }
+
+            Console.WriteLine(VIP.Count + regular.Count);
+
+            foreach (var guest in VIP)
+            {
+                Console.WriteLine(guest);
+            }
+
+            foreach (var guest in regular)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+
+        static bool VIPChecker(string input)
+        {
+            return char.IsDigit(input[0]);
+        }
+    }
+}
