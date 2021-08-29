f761c7a158cfe3623e5943f3c5ad60b1b5617093
unknown <blgv@abv.bg>
2021-08-29 16:49:26 +0300

    Added last 2 exercises from Sets and Dictionaries

---
+++ C#_Advanced/#8_Sets_and_Dictionaries_Advanced_Exercise/07. The_V-Logger/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._The_V_Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, SortedSet<string>>> vLogger =
+                new Dictionary<string, Dictionary<string, SortedSet<string>>>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "Statistics")
+            {
+                string[] tokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string vloggerName = tokens[0];
+                string command = tokens[1];
+
+                switch (command)
+                {
+                    case "joined":
+
+                        if (vLogger.ContainsKey(vloggerName) == false)
+                        {
+                            vLogger.Add(vloggerName, new Dictionary<string, SortedSet<string>>());
+                            vLogger[vloggerName].Add("following", new SortedSet<string>());
+                            vLogger[vloggerName].Add("followers", new SortedSet<string>());
+                        }
+
+                        break;
+
+                    case "followed":
+
+                        string followName = tokens[2];
+
+                        if (!vLogger.ContainsKey(vloggerName) ||
+                            !vLogger.ContainsKey(followName) ||
+                            vloggerName == followName ||
+                            vLogger[vloggerName]["following"].Any(x => x == followName))
+                        {
+                            continue;
+                        }
+
+                        vLogger[vloggerName]["following"].Add(followName);
+                        vLogger[followName]["followers"].Add(vloggerName);
+
+                        break;
+                }
+            }
+
+            Console.WriteLine($"The V-Logger has a total of {vLogger.Count} vloggers in its logs.");
+
+            int counter = 1;
+
+            foreach (var vloggers in vLogger.OrderByDescending(x => x.Value["followers"].Count).ThenBy(x => x.Value["following"].Count))
+            {
+                if (vloggers.Value["followers"].Count != 0 && counter == 1)
+                {
+                    Console.WriteLine($"{counter}. {vloggers.Key} : {vloggers.Value["followers"].Count} followers, {vloggers.Value["following"].Count} following");
+
+                    foreach (var item in vloggers.Value["followers"])
+                    {
+                        Console.WriteLine($"*  {item}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{counter}. {vloggers.Key} : {vloggers.Value["followers"].Count} followers, {vloggers.Value["following"].Count} following");
+                }
+                counter++;
+            }
+        }
+    }
+}
---
+++ C#_Advanced/#8_Sets_and_Dictionaries_Advanced_Exercise/08. Ranking/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Ranking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> contests = new Dictionary<string, string>();
+            SortedDictionary<string, Dictionary<string, int>> users = new SortedDictionary<string, Dictionary<string, int>>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "end of contests")
+            {
+                string[] tokens = input.Split(':', StringSplitOptions.RemoveEmptyEntries);
+                string contest = tokens[0];
+                string password = tokens[1];
+
+                if (contests.ContainsKey(contest) == false)
+                {
+                    contests.Add(contest, password);
+                }
+            }
+
+            while ((input = Console.ReadLine()) != "end of submissions")
+            {
+                string[] tokens = input.Split("=>", StringSplitOptions.RemoveEmptyEntries);
+                string contest = tokens[0];
+                string password = tokens[1];
+                string username = tokens[2];
+                int points = int.Parse(tokens[3]);
+
+                if (contests.ContainsKey(contest) == false ||
+                    contests[contest] != password)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (users.ContainsKey(username) == false)
+                    {
+                        users.Add(username, new Dictionary<string, int>());
+                    }
+
+                    if (users[username].ContainsKey(contest) == false)
+                    {
+                        users[username].Add(contest, 0);
+                    }
+
+
+                    if (users[username][contest] < points)
+                    {
+                        users[username][contest] = points;
+                    }
+                }
+            }
+
+            int maxPoints = 0;
+            string maxUser = string.Empty;
+
+            foreach (var (key, value) in users)
+            {
+                int current = 0;
+
+                foreach (var item in value)
+                {
+                    current += item.Value;
+                }
+
+                if (maxPoints < current)
+                {
+                    maxPoints = current;
+                    maxUser = key;
+                }
+
+            }
+
+            Console.WriteLine($"Best candidate is {maxUser} with total {maxPoints} points.");
+            Console.WriteLine("Ranking:");
+
+            foreach (var (key, value) in users)
+            {
+                Console.WriteLine($"{key}");
+
+                foreach (var item in value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"#  {item.Key} -> {item.Value}");
+                }
+            }
+        }
+    }
+}
