7914a3f27cfb03a8442ab5adcabcc2f586ee1181
unknown <blgv@abv.bg>
2021-08-31 12:11:51 +0300

    Added rest of Streams, Files and Directories Lab

---
+++ C#_Advanced/#9_Streams_Files_And_Directories_Lab/05. SliceAFile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _05._SliceFile
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int parts = 4;
+            byte[] buffer = new byte[4096];
+            int totalBytes = 0;
+
+            using (FileStream fs = new FileStream("sliceMe.txt", FileMode.Open, FileAccess.Read))
+            {
+                int partSize = (int)Math.Ceiling((double)fs.Length / parts);
+
+                for (int i = 1; i <= parts; i++)
+                {
+                    int readBytes = 0;
+
+                    using (FileStream ofs = new FileStream($"Part-{i}.txt", FileMode.Create, FileAccess.Write))
+                    {
+                        while (readBytes < partSize && totalBytes < fs.Length)
+                        {
+                            int bytesToRead = Math.Min(buffer.Length, partSize - readBytes);
+                            int bytes = await fs.ReadAsync(buffer, 0, bytesToRead);
+                            await ofs.WriteAsync(buffer, 0, bytes);
+                            readBytes += bytes;
+                            totalBytes += bytes;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C#_Advanced/#9_Streams_Files_And_Directories_Lab/06. FolderSize/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _06._FolderSize
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string[] fileNames = Directory.GetFiles("TestFolder");
+            double totalSize = 0;
+
+            foreach (var fileName in fileNames)
+            {
+                FileInfo info = new FileInfo(fileName);
+                totalSize += info.Length;
+            }
+
+            totalSize = totalSize / 1024 / 1024;
+
+            //Console.WriteLine(totalSize);
+
+            await File.WriteAllTextAsync("output.txt", totalSize.ToString());
+        }
+    }
+}
