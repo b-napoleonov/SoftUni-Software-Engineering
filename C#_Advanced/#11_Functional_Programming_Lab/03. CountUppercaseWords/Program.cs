b3033c4da99090d4ed3061091db550ca50150390
unknown <blgv@abv.bg>
2021-09-03 16:22:14 +0300

    Added 4 exercises from Functional Programming Lab

---
+++ C#_Advanced/#11_Functional_Programming_Lab/03. CountUppercaseWords/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> isUpprer = l => char.IsUpper(l[0]);
+
+            Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(l => isUpprer(l))
+                .ToList()
+                .ForEach(l => Console.WriteLine(l));
+        }
+    }
+}
---
+++ C#_Advanced/#11_Functional_Programming_Lab/04. AddVAT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(n => n * 1.2)
+                .ToList()
+                .ForEach(n => Console.WriteLine($"{n:F2}"));
+        }
+    }
+}
