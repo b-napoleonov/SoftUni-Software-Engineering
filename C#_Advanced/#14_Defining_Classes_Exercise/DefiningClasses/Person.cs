255c88c6b4fab4b1352a847cc9edc1603fc232e8
unknown <blgv@abv.bg>
2021-09-15 09:09:38 +0300

    Added Exercise 1 to 4 from Defining Classes Exercise

---
+++ C#_Advanced/#14_Defining_Classes_Exercise/DefiningClasses/Person.cs
@@ -0,0 +1,27 @@
+﻿namespace DefiningClasses
+{
+    public class Person
+    {
+        public Person()
+        {
+
+        }
+        public Person(int age)
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age)
+            :this(age)
+        {
+            Name = name;
+        }
+        public string Name { get; set; } = "No name";
+        public int Age { get; set; } = 1;
+
+        public override string ToString()
+        {
+            return $"{Name} - {Age}";
+        }
+    }
+}
---
+++ C#_Advanced/#14_Defining_Classes_Exercise/DefiningClasses/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] member = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Person current = new Person(member[0], int.Parse(member[1]));
+                family.AddMember(current);
+            }
+
+            family.FamilyMembers.Where(m => m.Age > 30).OrderBy(m => m.Name).ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
