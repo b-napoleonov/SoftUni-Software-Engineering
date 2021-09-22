e3fa30c81d8e62bbb7a87c470caadd92fb25762e
unknown <blgv@abv.bg>
2021-09-22 10:22:31 +0300

    Added Iterators and Comparators Lab

---
+++ C#_Advanced/#19_Iterators_And_Comparators_Lab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private readonly SortedSet<Book> books;
+        public Library(params Book[] _books)
+        {
+            books = new SortedSet<Book>(_books, new BookComparator());
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly IList<Book> books;
+
+            private int index;
+            public Book Current => books[index];
+
+            object IEnumerator.Current => Current;
+
+            public LibraryIterator(IEnumerable<Book> _books)
+            {
+                Reset();
+                books = new List<Book>(_books);
+            }
+
+            public void Dispose()
+            {
+
+            }
+
+            public bool MoveNext()
+            {
+                return ++index < books.Count;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+        }
+    }
+}
