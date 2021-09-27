9f5d23c749212696f02b3d5fb50be0216c7e6f41
unknown <blgv@abv.bg>
2021-09-27 08:09:56 +0300

    Added Iterators and Comparators Exercise

---
+++ C#_Advanced/#20_Iterators_And_Comparators_Exercise/ComparingObjects/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+        public int CompareTo(Person other)
+        {
+            if (Name.CompareTo(other.Name) != 0)
+            {
+                return Name.CompareTo(other.Name);
+            }
+
+            if (Age.CompareTo(other.Age) != 0)
+            {
+                return Age.CompareTo(other.Age);
+            }
+
+            if (Town.CompareTo(other.Town) != 0)
+            {
+                return Town.CompareTo(other.Town);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ C#_Advanced/#20_Iterators_And_Comparators_Exercise/Froggy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lake stones = new Lake(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+            Console.WriteLine(string.Join(", ", stones));
+        }
+    }
+}
