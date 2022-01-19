c33186b8616a5df1ba465474a09e34d040345f33
unknown <blgv@abv.bg>
2022-01-19 11:03:51 +0200

    Slight change to first exercise from Objects and Composition

--- JS Advanced/8_Objects_And_Composition_Exercise/01.CalorieObject.js
+++ JS Advanced/8_Objects_And_Composition_Exercise/01.CalorieObject.js
@@ -1,22 +1,12 @@
 function calorieObject(arr){
-    let name = [];
-    let calories = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (i % 2 === 0) {
-            name.push(arr[i]);
-        }
-        else {
-            calories.push(arr[i]);
-        }
-    }
-
     let obj = {};
-    for (let i = 0; i < name.length; i++) {
-        obj[name[i]] = Number(calories[i]);
-    }
 
-    console.log(obj);
+    arr.forEach((_, i) => {
+       i % 2 === 0 ? obj[arr[i]] = Number(arr[i + 1]) : null; 
+    });
+
+    return obj;
 }
 
-calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']);
-calorieObject(['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42']);
+console.log(calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']));
+console.log(calorieObject(['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42']));
