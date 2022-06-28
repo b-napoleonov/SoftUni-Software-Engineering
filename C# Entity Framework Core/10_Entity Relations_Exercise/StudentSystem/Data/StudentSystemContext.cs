92c926ea4a6c9d6a0922b9bd5cad6ab7a209d9ca
Blagovest Napoleonov <blgv@abv.bg>
2022-06-28 14:00:07 +0300

    Added Entity Relations - Exercise

---
+++ C# Entity Framework Core/10_Entity Relations_Exercise/StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+
+        public StudentSystemContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+
+        public DbSet<Resource> Resources { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=SoftUni;User Id=sa;Password=adminPass1;");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>(x =>
+            {
+                x.HasKey(k => new { k.CourseId, k.StudentId });
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
